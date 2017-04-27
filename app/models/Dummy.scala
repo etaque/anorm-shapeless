f46b68227547a0e45e8c520d6d1821b1292875f7
Emilien Taque <eta@zengularity.com>
2017-04-27 14:40:01 +0200

    almost working

--- app/models/Dummy.scala
+++ app/models/Dummy.scala
@@ -3,51 +3,130 @@ package models
 import anorm._
 import shapeless._
 import shapeless.record._
-
-trait ParamBuilder[A] {
-  def toParam(value: A): Seq[NamedParameter]
-}
+import shapeless.ops.record._
+import shapeless.labelled.FieldType
 
 case class Dummy(
   someInt: Int,
   someString: String
 )
 
-object namedParameterPoly extends Poly2 {
-  implicit val intCase: Case.Aux[Symbol, Int, NamedParameter] =
-    at((k, v) => NamedParameter(k.name, ParameterValue.toParameterValue(v)))
 
-  implicit val stringCase: Case.Aux[Symbol, String, NamedParameter] =
-    at((k, v) => NamedParameter(k.name, ParameterValue.toParameterValue(v)))
-
-  // implicit val stringCase: Case.Aux[String, ParameterValue] =
-  //   at(ParameterValue.toParameterValue(_))
+trait ParamsEncoder[A] {
+  def encode(value: A): Seq[NamedParameter]
 }
 
-object Dummy {
-  implicit val paramBuilder: ParamBuilder[Dummy] = new ParamBuilder[Dummy] {
-    def toParam(value: Dummy): Seq[NamedParameter] = {
-      val p: String = LabelledGeneric[Dummy].to(value)
-      LabelledGeneric[Dummy].to(value).map(namedParameterPoly)
+object ParamsEncoder {
+  def apply[A](implicit enc: ParamsEncoder[A]): ParamsEncoder[A] = enc
 
-      // LabelledGeneric[Dummy].to(value)
-      //   .toMap.toSeq.map { case (key, value) =>
-      //   // NamedParameter(key.name, value)
-      // }
-      ???
-
+  def createEncoder[A](fn: A => Seq[NamedParameter]): ParamsEncoder[A] =
+    new ParamsEncoder[A] {
+      def encode(value: A): Seq[NamedParameter] =
+        fn(value)
     }
-    // Seq[NamedParameter](
-    //   'someInt -> value.someInt,
-    //   'someString -> value.someString
-    // )
+
+  implicit val hnilEncoder: ParamsEncoder[HNil] =
+    createEncoder(hnil => Nil)
+
+  implicit def hlistParamsEncoder[K <: Symbol, H, T <: HList](
+    implicit
+      witness: Witness.Aux[K],
+    toSql: Lazy[ToSql[H]],
+    toStmt: Lazy[ToStatement[H]],
+    tEncoder: ParamsEncoder[T]
+  ): ParamsEncoder[FieldType[K, H] :: T] = {
+    val fieldName: String = witness.value.name
+    createEncoder { hlist =>
+      val value: H = hlist.head
+      val head = ParameterValue.toParameterValue(value)(toSql.value, toStmt.value) //hEncoder.value.encode(hlist.head)
+      val tail = tEncoder.encode(hlist.tail)
+      NamedParameter(fieldName, head) +: tail
+    }
   }
+
+  implicit def genericParamsEncoder[A, H <: HList](
+    implicit
+      generic: LabelledGeneric.Aux[A, H],
+    hEncoder: Lazy[ParamsEncoder[H]]
+  ): ParamsEncoder[A] =
+    createEncoder { value =>
+      hEncoder.value.encode(generic.to(value))
+    }
 }
 
 object TestDAO {
-  def insert[A](value: A)(implicit pb: ParamBuilder[A]): Unit = {
-    pb.toParam(value)
-  }
+  val dummyGen = LabelledGeneric[Dummy]
+  ParamsEncoder[Dummy].encode(Dummy(1, "test"))
+  // def insert[A](value: A)(implicit pb: ParamBuilder[A]): Unit = {
+  //   pb.toParam(value)
+  // }
 
-  insert(Dummy(1, "test"))
+  // insert(Dummy(1, "test"))
 }
+
+
+
+
+
+
+
+
+// trait ParamBuilder[A] {
+//   def toParam(value: A): Seq[NamedParameter]
+// }
+
+// object ParamBuilder {
+//   def apply[A](implicit pb: ParamBuilder[A]): ParamBuilder[A] = pb
+// }
+
+
+
+
+// object npPoly extends Poly1 {
+//   // implicit val intCase: Case.Aux[Int with shapeless.labelled.KeyTag[_, _], ParameterValue] =
+//   //   at(ParameterValue.toParameterValue(_))
+
+// }
+
+// object pvPoly extends Poly1 {
+//   implicit val intCase: Case.Aux[Int, ParameterValue] =
+//     at(ParameterValue.toParameterValue(_))
+
+//   implicit val stringCase: Case.Aux[String, ParameterValue] =
+//     at(ParameterValue.toParameterValue(_))
+// }
+
+// object toName extends Poly1 {
+//   implicit def keyToName[A] = at[Symbol with A](_.name)
+// }
+
+// object Dummy {
+//   implicit val paramBuilder: ParamBuilder[Dummy] = new ParamBuilder[Dummy] {
+//     def toParam(value: Dummy): Seq[NamedParameter] = {
+//       // val p: String = LabelledGeneric[Dummy].to(value)
+//       val lGen = LabelledGeneric[Dummy]
+//       val repr = lGen.to(value)
+//       repr.values.map(pvPoly)
+//       Keys[lGen.Repr]
+
+//       // LabelledGeneric[Dummy].to(value)
+//       //   .toMap.toSeq.map { case (key, value) =>
+//       //   // NamedParameter(key.name, value)
+//       // }
+//       ???
+
+//     }
+//     // Seq[NamedParameter](
+//     //   'someInt -> value.someInt,
+//     //   'someString -> value.someString
+//     // )
+//   }
+// }
+
+// object TestDAO {
+//   def insert[A](value: A)(implicit pb: ParamBuilder[A]): Unit = {
+//     pb.toParam(value)
+//   }
+
+//   insert(Dummy(1, "test"))
+// }
