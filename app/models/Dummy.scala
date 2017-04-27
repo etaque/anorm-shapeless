65141e36e5cd194330f516814fd348430891d3f8
Emilien Taque <eta@zengularity.com>
2017-04-27 12:15:32 +0200

    stuff

--- app/models/Dummy.scala
+++ app/models/Dummy.scala
@@ -1,10 +1,53 @@
 package models
 
+import anorm._
+import shapeless._
+import shapeless.record._
+
+trait ParamBuilder[A] {
+  def toParam(value: A): Seq[NamedParameter]
+}
+
 case class Dummy(
   someInt: Int,
   someString: String
 )
 
+object namedParameterPoly extends Poly2 {
+  implicit val intCase: Case.Aux[Symbol, Int, NamedParameter] =
+    at((k, v) => NamedParameter(k.name, ParameterValue.toParameterValue(v)))
+
+  implicit val stringCase: Case.Aux[Symbol, String, NamedParameter] =
+    at((k, v) => NamedParameter(k.name, ParameterValue.toParameterValue(v)))
+
+  // implicit val stringCase: Case.Aux[String, ParameterValue] =
+  //   at(ParameterValue.toParameterValue(_))
+}
+
 object Dummy {
-  
+  implicit val paramBuilder: ParamBuilder[Dummy] = new ParamBuilder[Dummy] {
+    def toParam(value: Dummy): Seq[NamedParameter] = {
+      val p: String = LabelledGeneric[Dummy].to(value)
+      LabelledGeneric[Dummy].to(value).map(namedParameterPoly)
+
+      // LabelledGeneric[Dummy].to(value)
+      //   .toMap.toSeq.map { case (key, value) =>
+      //   // NamedParameter(key.name, value)
+      // }
+      ???
+
+    }
+    // Seq[NamedParameter](
+    //   'someInt -> value.someInt,
+    //   'someString -> value.someString
+    // )
+  }
+}
+
+object TestDAO {
+  def insert[A](value: A)(implicit pb: ParamBuilder[A]): Unit = {
+    pb.toParam(value)
+  }
+
+  insert(Dummy(1, "test"))
 }
--- build.sbt
+++ build.sbt
@@ -1,32 +1,18 @@
-name := "wanaboat"
+name := "anorm-shapeless"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  filters,
   "org.postgresql" % "postgresql" % "9.4-1201-jdbc41",
   jdbc,
-  ws,
   "com.typesafe.play" %% "anorm" % "2.5.3",
-  "org.apache.lucene" % "lucene-core" % "6.3.0",
-  "org.mindrot" % "jbcrypt" % "0.3m",
-  "org.flywaydb" %% "flyway-play" % "2.2.1",
-  "com.typesafe.play" %% "play-mailer" % "3.0.1",
-  "commons-io" % "commons-io" % "2.4"
+  "com.chuusai" %% "shapeless" % "2.3.2"
 )
 
 play.sbt.PlayScala.projectSettings
 
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfatal-warnings", "-Xlint")
-
-TwirlKeys.templateImports ++= Seq("models._", "views._")
-
-//initialCommands in console := """play.core.server.ProdServerStart.main(Array())"""
-
-// offline := true
-
-// skip in update := true
 
 routesGenerator := InjectedRoutesGenerator
